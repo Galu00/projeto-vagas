2ec107277b882437a361cc84ae01497a8ad26cbe
Gabriel Galindo <gabrielgalindo1m@gmail.com>
2023-10-25 22:41:28 -0300

    First Step

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div className="container">
+      <h1 className='text-white'>
+        Projeto de Vagas
+      </h1>
+    </div>
+  </React.StrictMode>,
+)
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.tsx',
+    './src/**/*.ts',    
+    './src/**/*.jsx',   
+    './src/**/*.js',    
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
